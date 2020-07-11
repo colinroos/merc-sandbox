cacc13b13ea1a6bc4365e837eadd09a7f63c30c9
colinroos <colin.a.roos@gmail.com>
2020-07-10 21:53:25 -0400

    Experiment with Hough Lines.

--- box_locate.py
+++ box_locate.py
@@ -4,10 +4,16 @@ from file_utils import *
 import constants as c
 
 
+def line_length(line):
+    return np.sqrt((line[0, 0] - line[0, 2]) ** 2 + (line[0, 1] - line[0, 3]) ** 2)
+
+
 class BoxLocate:
     def __init__(self):
         self.image = cv.imread('data/2017/images/KAD17-001_Bx1-5_11.5-25.30m_DxO.jpg')
+        self.grayscale = cv.cvtColor(self.image, cv.COLOR_BGR2GRAY)
         self.processed = self.image.copy()
+        self.processed2 = self.image.copy()
         self.out_img = self.image.copy()
 
         # Edges groups
@@ -21,26 +27,45 @@ class BoxLocate:
 
     def run(self):
         # Process image
-        ret, self.processed = cv.threshold(cv.cvtColor(self.image, cv.COLOR_BGR2GRAY), c.THRESHOLD, 255, cv.THRESH_BINARY)
+        self.grayscale = cv.cvtColor(self.image, cv.COLOR_BGR2GRAY)
+
+        # self.processed = cv.adaptiveThreshold(self.grayscale, 0, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 3, 0.01)
+        self.processed = cv.GaussianBlur(self.grayscale, (7, 7), 0)
+        self.processed = cv.Canny(self.grayscale, 150, 190, apertureSize=3)
+        self.processed = cv.GaussianBlur(self.processed, (3, 3), 0)
+        # ret, self.processed2 = cv.threshold(self.grayscale, 180, 255, cv.THRESH_BINARY_INV)
+        # self.processed = cv.add(self.processed, self.processed2)
         self.out_img = cv.cvtColor(self.processed, cv.COLOR_GRAY2BGR)
 
-        self.find_top()
+        v_lines = cv.HoughLinesP(self.processed, 2, np.pi, 200, 150, 100)
+        h_lines = cv.HoughLinesP(self.processed, 2, np.pi/180, 200, 150, 100)
 
-        top_edges = []
-        top_edges.extend(self.left_edges)
-        top_edges.extend(self.right_edges)
+        if len(v_lines) != 0:
+            for line in v_lines[:, 0, :]:
+                cv.line(self.out_img, (line[0], line[1]), (line[2], line[3]), c.COLORS[1], 2)
 
-        edges_x = [edge[0] for edge in top_edges]
-        edges_y = [edge[1] for edge in top_edges]
-        ret = np.polyfit(edges_x, edges_y, 1)
+        if len(h_lines) != 0:
+            for line in h_lines[:, 0, :]:
+                cv.line(self.out_img, (line[0], line[1]), (line[2], line[3]), c.COLORS[2], 2)
 
-        m, b = ret[0], ret[1]
+        # self.find_top()
 
-        self.top_edge = Line(Point(0, b), slope=m)
+        # top_edges = []
+        # top_edges.extend(self.left_edges)
+        # top_edges.extend(self.right_edges)
 
-        cv.line(self.out_img, (0, int(b)), (self.out_img.shape[1], int(self.out_img.shape[1] * m + b)), (0, 0, 255), 5)
+        # edges_x = [edge[0] for edge in top_edges]
+        # edges_y = [edge[1] for edge in top_edges]
+        # ret = np.polyfit(edges_x, edges_y, 1)
 
-        cv.imshow('out', self.out_img)
+        # m, b = ret[0], ret[1]
+
+        # self.top_edge = Line(Point(0, b), slope=m)
+
+        # cv.line(self.out_img, (0, int(b)), (self.out_img.shape[1], int(self.out_img.shape[1] * m + b)), (0, 0, 255), 5)
+
+        print('processed')
+        cv.imshow('out', cv.resize(self.out_img, (0, 0), fx=0.25, fy=0.25))
         cv.waitKey(0)
 
         return self.out_img
@@ -58,10 +83,10 @@ class BoxLocate:
             if len(l_edges) == 0 or len(r_edges) == 0:
                 continue
 
-            for edge in l_edges[:8]:
+            for edge in l_edges[:5]:
                 left_edges.append([left_detector, edge])
 
-            for edge in r_edges[:8]:
+            for edge in r_edges[:5]:
                 right_edges.append([right_detector, edge])
 
         matched_blobs = []
@@ -70,7 +95,7 @@ class BoxLocate:
             # for edge in list_of_edges:
             #     cv.circle(self.out_img, (edge[0], edge[1]), 5, (0, 255, 0), -1)
 
-            blobs, centroids = cluster_edges(list_of_edges, axis=1, max_clusters=20, locate_elbow=True)
+            blobs, centroids = cluster_edges(list_of_edges, axis=1, max_clusters=8, locate_elbow=True)
 
             # for blob, color in zip(blobs, c.COLORS[:len(blobs)]):
             #     for edge in blob:
